d54da8ccecd35d7711411c73e226039a9af78227
Thinh Mai <thinh.quocc@gmail.com>
2025-07-07 17:29:18 +0700

    add index - app

---
+++ src/problem2/src/App.jsx
@@ -0,0 +1,9 @@
+import CurrencySwapForm from './components/CurrencySwapForm'
+
+export default function App() {
+  return (
+    <div className="app-container">
+      <CurrencySwapForm />
+    </div>
+  )
+}
